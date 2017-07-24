d878b4e169f9bd3b9ee7b29869eba71b60cad9f5
Allie Grampa <agrampa@yahoo.com>
2017-07-24 15:42:32 -0700

    added basic template for projects

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+class Projects extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <p>Title: {this.props.title}</p>
+      <p>Description: {this.props.description}</p>
+      <p>Deployed on: {this.props.deployDate}</p>
+      <p>Link to deployed site: {this.props.deployedLink}</p>
+      <p>Link to repository on GitHub: {this.props.repoLink}</p>
+    )
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
